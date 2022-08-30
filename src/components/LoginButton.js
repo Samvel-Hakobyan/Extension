04d93bb5a619b10f21a009edef69514b6971fb68
Yunloop-Dev <benbosse@yandex.ru>
2022-08-30 22:13:44 +0300

    code review

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginButton = () => {
-    const { loginWithRedirect, isAuthenticated } = useAuth0();
-    return (
-        !isAuthenticated && (
-            <button className="login-button" onClick={()=> loginWithRedirect()}>
-                Login
-            </button>
-        )
-    );
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  return (
+    !isAuthenticated && (
+      <button className="login-button" onClick={() => loginWithRedirect()}>
+        Login
+      </button>
+    )
+  );
 };
 
 export default LoginButton;
--- src/request/UrlRequest.js
+++ src/request/UrlRequest.js
@@ -1,26 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function UrlRequest() {
-    const [urls, setUrl] = useState([])
-    axios.get("https://630b3c26f280658a59d82ec1.mockapi.io/url")
-            .then(res => {
-                setUrl(res.data)
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    return (
-        <div className="card-content">
-            {urls.length > 0 && (
-                <ul>
-                    {urls.map(url => (
-                        <li key={url.id}>{url.url}</li>
-                    ))}
-                </ul>
-            )}
-        </div>
-    );
+  const [urls, setUrl] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://630b3c26f280658a59d82ec1.mockapi.io/url")
+      .then((res) => {
+        setUrl(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div className="card-content">
+      {urls.length > 0 && (
+        <ul>
+          {urls.map((url) => (
+            <li key={url.id}>{url.url}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default UrlRequest;
